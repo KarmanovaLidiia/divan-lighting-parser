e3dc4671ce861bc421bfb76e7980d4a154ed2cbc
KarmanovaLidiia <150813074+KarmanovaLidiia@users.noreply.github.com>
2025-06-29 23:21:48 +0700

    .

--- divanpars/spiders/divannewpars.py
+++ divanpars/spiders/divannewpars.py
@@ -1,17 +1,15 @@
 import scrapy
 
-class DivannewparsSpider(scrapy.Spider):
-    name = "divannewpars"
+class DivanSpider(scrapy.Spider):
+    name = "divan"
     allowed_domains = ["divan.ru"]
-    start_urls = ["https://www.divan.ru/category/divany-i-kresla"]
+    start_urls = ["https://www.divan.ru/category/divany"]
 
     def parse(self, response):
-        # Получаем все карточки диванов
-        divans = response.css('div[data-testid="product-card"]')
-
-        for divan in divans:
+        items = response.css('div[data-testid="product-card"]')
+        for item in items:
             yield {
-                'name': divan.css('a::text').get(),  # название
-                'price': divan.css('meta[itemprop="price"]::attr(content)').get(),  # цена
-                'url': divan.css('a::attr(href)').get()  # ссылка
+                'name': item.css('span[itemprop="name"]::text').get(),
+                'price': item.css('meta[itemprop="price"]::attr(content)').get(),
+                'url': response.urljoin(item.css('a::attr(href)').get())
             }
